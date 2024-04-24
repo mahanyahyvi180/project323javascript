133bb64c6a5209c4d42add59a854257ce95f9bca
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2024-04-24 21:07:44 +0330

    Add files via upload

---
+++ web java2/js/text.js
@@ -0,0 +1,47 @@
+var myPara=document.getElementById("content").getElementsByTagName("p")[5];
+var myPara2=$("#content p:last-child");
+/* var myForm=document.forms.myForm;
+var message=document.getElementById("message");
+
+myForm.onsubmit=function(){
+    if(myForm.name.value==""){
+        message.innerHTML="please enter a name";
+        return false;
+    }else{
+        message.innerHTML="";
+        return true;
+    }
+}; */
+/* var colourChanger=document.getElementById("colour-changer");
+var colours=["red","blue","green","pink"];
+var counter=0;
+
+function changeColour(){
+    if(counter>=colours.length){
+        counter=0;
+    }
+    colourChanger.style.background=colours[counter];
+    counter++;
+}
+var myTimer=setInterval(changeColour,3000);
+
+colourChange=onclick=function(){
+    clearInterval(myTimer);
+    colourChanger.innerHTML="timer out";
+}; */
+/* var colourChanger=document.getElementById("colour-changer");
+var colours=["red","blue","green","pink"];
+var counter=0;
+
+function changeColour(){
+    colourChanger.style.background=colours[counter];
+    counter++;
+}
+setInterval(changeColour,3000); */
+/* var myMessage=document.getElementById("message");
+
+function showMessage(){
+    myMessage.className="show";
+}
+
+setTimeout(showMessage,3000) ; */
